ec299cf23736b5398c842c6f2ca7757e373da67f
tedtasman <tktasman@gmail.com>
2024-03-18 00:18:18 -0400

    this sucks

--- src/App.js
+++ src/App.js
@@ -1,27 +1,3 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
 export default function Homepage() {
   return (
     <>
@@ -63,6 +39,7 @@ function Drawer({ value }) {
 function Shelf() {
   return (
     <div className='Shelf'>
+      <Drawer value="Under Construction" />
       <Drawer value="Under Construction" />
       <Drawer value="Under Construction" />
       <Drawer value="Under Construction" />
