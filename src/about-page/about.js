a8b986d46615203d227893c44e131ad099887a1d
Ted Tasman <tktasman@gmail.com>
2024-05-30 18:54:15 -0400

    add body to about page; need images

--- src/about-page/about.js
+++ src/about-page/about.js
@@ -1,6 +1,8 @@
 import { Footer } from "../footer";
 import { Header } from "../header2";
-import './about.modules.css'
+import './about.modules.css';
+import about from './content/about.json'
+
 
 export default function About() {
 
@@ -12,6 +14,21 @@ export default function About() {
                     <h1>About Me</h1>
                     <h2>Insight into my interests, motivations, and goals</h2>
                     <h1><i class="fa-solid fa-chevron-down fa-xl"></i></h1>
+                </div>
+            </div>
+            <div className="About-background">
+                <div className="About-core">
+                    <h2 style={{fontWeight: 'bold'}}>{about.title}</h2>
+                    <p>{about.introduction}</p>
+                    <p>{about.personal_experiences}</p>
+                    <p>{about.interests}</p>
+                    <p>{about.conclusion}</p>
+                    <p>{about.closing}</p>
+                </div>
+                <div className="About-photo-container">
+                    <img src={psu} alt='Penn State University Logo' className='Experience-image'/>
+                    <img src={psu} alt='Penn State University Logo' className='Experience-image'/>
+                    <img src={psu} alt='Penn State University Logo' className='Experience-image'/>
                 </div>
             </div>
             <Footer />
