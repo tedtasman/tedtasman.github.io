46e97bcbedcc66d58e9d66efbc5266e6596d0d37
Ted Tasman <tktasman@gmail.com>
2024-06-04 14:40:38 -0400

    make about page

--- src/about-page/about.js
+++ src/about-page/about.js
@@ -2,6 +2,9 @@ import { Footer } from "../footer";
 import { Header } from "../header2";
 import './about.modules.css';
 import about from './content/about.json'
+import benji from './content/benji.jpg'
+import guitar from './content/guitar.jpeg'
+import bike from './content/bike.jpeg'
 
 
 export default function About() {
@@ -9,26 +12,28 @@ export default function About() {
     return (
         <>
             <Header />
-            <div className='About-landing'>
-                <div className='Page-title'>
-                    <h1>About Me</h1>
-                    <h2>Insight into my interests, motivations, and goals</h2>
-                    <h1><i class="fa-solid fa-chevron-down fa-xl"></i></h1>
+            <div className="Page-background">
+                <div className='About-landing'>
+                    <div className='Page-title'>
+                        <h1>About Me</h1>
+                        <h2>Insight into my interests, motivations, and goals</h2>
+                        <h1><i class="fa-solid fa-chevron-down fa-xl"></i></h1>
+                    </div>
                 </div>
-            </div>
-            <div className="About-background">
-                <div className="About-core">
-                    <h2 style={{fontWeight: 'bold'}}>{about.title}</h2>
-                    <p>{about.introduction}</p>
-                    <p>{about.personal_experiences}</p>
-                    <p>{about.interests}</p>
-                    <p>{about.conclusion}</p>
-                    <p>{about.closing}</p>
-                </div>
-                <div className="About-photo-container">
-                    <img src={psu} alt='Penn State University Logo' className='Experience-image'/>
-                    <img src={psu} alt='Penn State University Logo' className='Experience-image'/>
-                    <img src={psu} alt='Penn State University Logo' className='Experience-image'/>
+                <div className="About-background">
+                    <div className="About-core">
+                        <h2 style={{fontWeight: 'bold'}}>{about.title}</h2>
+                        <p>{about.introduction}</p>
+                        <p>{about.personal_experiences}</p>
+                        <p>{about.interests}</p>
+                        <p>{about.conclusion}</p>
+                        <p>{about.closing}</p>
+                    </div>
+                    <div className="About-photo-container">
+                        <img src={bike} alt='Image of bike'/>
+                        <img src={guitar} alt='Image of guitar'/>
+                        <img src={benji} alt='Image of dog'/>
+                    </div>
                 </div>
             </div>
             <Footer />
