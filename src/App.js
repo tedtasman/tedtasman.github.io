3dc6bd1fd801582c6b66769513dae9a692ccc18b
Ted Tasman <tktasman@gmail.com>
2024-07-19 19:04:41 -0400

    start v2

--- src/App.js
+++ src/App.js
@@ -1,19 +1,10 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Homepage from './home-page/homepage';
-import Projects from './projects-page/projects'
-import About from './about-page/about';
-import Experience from './experience-page/experience';
-
+import './App.css';
 
 export default function App() {
   return (
-    <Router>
-      <Routes>
-        <Route exact path="/" element={<Homepage />} />
-        <Route path="/projects" element={<Projects />} />
-        <Route path='/about' element={<About />} />
-        <Route path='/experience' element={<Experience />} />
-      </Routes>
-    </Router>
+    <div className="Wrapper">
+      <h1>React App</h1>
+      <p>React is a JavaScript library for building user interfaces</p>
+    </div>
   );
 }
