8ee7420d2f5c6607a4bcfd5ace3cc912fcf6eb87
tedtasman <tktasman@gmail.com>
2025-01-25 20:42:30 -0500

    scroll to top

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import About from './About';
 import Experience from './Experience';
 import Projects from './Projects';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import ScrollToTop from './ScrollToTop';
 
 export default function App() {
 
   return (
     <Router basename='/'>
+      <ScrollToTop />
       <Routes>
         <Route exact path="/" element={<Base />} />
         <Route path="/about" element={<About />} />
